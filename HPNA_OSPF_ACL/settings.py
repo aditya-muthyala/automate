1c185e05815094d0a8c5c084433110103a57b182
aditya <am3551@columbia.edu>
2014-08-04 15:57:02 -0700

    changing general tag

--- HPNA_OSPF_ACL/settings.py
+++ HPNA_OSPF_ACL/settings.py
@@ -5,26 +5,9 @@ Add to 'generalTag' list the regular expression to tag the respective VIPs
 as general with tag 595. the dictionary tagMap contains the regex
 used by VserverIPHandler to add the VIPs to respective ACL number
 '''
-generalTag = ['VLAN',
-			'login',
-			'dns',
-			'builds',
-			'devices.col',
-			'-ats.',
-			'joing2',
-			'launch',
-			'logging',
-			'authentication',
-			'oauthsvc',
-			'commmom',
-			'commsession',
-			'commadmin',
-			'queue',
-			'depo',
-			'mdm',
+generalTag = ['dns',
 			'probe',
 			'vip',
-			'auth.*443'
 			]
 
 tagMap = {}
